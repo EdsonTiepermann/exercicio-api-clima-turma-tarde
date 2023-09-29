39b0ef885f8541b957f5b4f17728259768af5141
gibsonsg002 <gabrielsantosgibson@gmail.com>
2023-09-29 18:01:14 -0300

    desafio 2909

--- thalise_gabriel/apiclima.js
+++ thalise_gabriel/apiclima.js
@@ -1,15 +1,32 @@
-const cidadeInput = document.querySelector('#cidade');
+        const cidade = document.querySelector('#cidade');
 
         //Pagamos o elemento do input button buscar
         const botao = document.querySelector('#buscar');
         
+        //funcao para verificar e alimentar os campos
+        const showData = function(result){
+            //o for in para tratarmos um objeto, o for in pega o resultado (result)
+            //e insere na variavel campo
+            for(const campo in result) {
+                //nesse if verifico dinamicamente se todos os campos da api
+                //eu utilizo nos iguais
+                if (document.querySelector('#' + campo)){
+                    //Pegamos dinamicamente o elemento dos imputs e passamos o
+                    //value dinamicamente, dizendo q o resultado é um arrayy e passando 
+                    //a variavel campo como posição
+                    document.querySelector('#' + campo).value = result[campo]
+                    //console.log(campo);
+                }
+            }
+        }
+
         //Evento criado para realizar o envio da requisição
         //o (e) é a captura do evento 
         botao.addEventListener('click',function(e)
     {
         //cep.addEventListener('blur',function(e)){
             //replace função para substituir um carater
-            let cidade = cidadeInput.value.repalce('-','');
+            let search = cidade.value;
         
             //crio o objeto com as prioridades do envio para o ajax
             const options = {
@@ -24,27 +41,22 @@ const cidadeInput = document.querySelector('#cidade');
             // paramtreos necessarios, como por exemplo op CORS
             //serve para dizer que esta trabalhando com servidores
             //diferentes, como se fosse uma permissao de acesso
-            fetch('https://api.hgbrasil.com/weather?format=json-cors&key=7d4793ad&city_name=${cidade}', options)
+            fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=7d4793ad&city_name=${search}/json/`, options)
         
             //quando utilizamos o fetch ele retorna uma promessa,
             //ele é assincrono, entao fazemos as verificaçoes abaixo
             //entao faça algo, nesse caso cria uma funçao onde prgo o
             //"response" a resposta, no formato jso
             .then(function(response) {
-                response.json();
-                return response.json
-            })
+                response.json()
+
                 //o json tbem retorna uma promessa, então precisamos
                 //verificar se deu certo
-                then(function(data) {
-                    document.querySelector('#cidade-dados').textContent = data.results.city_name;
-                    document.querySelector('#data-dados').textContent = data.results.date;
-                    document.querySelector('#hora-dados').textContent = data.results.time;
-                    document.querySelector('#temperatura-dados').textContent = '${data.results.temp}ºC';
-                    document.querySelector('#condicao-dados').textContent = data.results.description;
-                    document.querySelector('#amanhecer-dados').textContent = data.results.sunrise;
-                    document.querySelector('#entardecer-dados').textContent = data.results.sunset;
+                .then(function(data) {
+                    showData(data)
+                    console.log(data)
                 })
+            })
         
             //se der errado faz outra coisa
             .catch(function(e) {
